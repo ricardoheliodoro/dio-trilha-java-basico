2ed81b7235ce3dbddb55762b5f67c433cd481b32
Ricardo Heliodoro <estudaricardo@gmail.com>
2023-09-08 03:31:54 -0300

    initial commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Apois viu!!");
+
+        int numero = -5;
+
+        System.out.println(numero);
+        System.out.println(-numero);
+        
+
+    }
+
+    //int, byte, short, long, float, double, boolean e char.
+}
---
+++ src/Testando.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Apois viu!!");
+
+        int numero = -5;
+
+        System.out.println(numero);
+        System.out.println(-numero);
+        
+
+    }
